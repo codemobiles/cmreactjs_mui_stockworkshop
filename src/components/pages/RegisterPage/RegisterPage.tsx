9533a7a3ba42dddc7ebdf735ba14a215988a1eda
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-03-15 09:55:05 +0700

    ep18 form and useState

--- src/components/pages/RegisterPage/RegisterPage.tsx
+++ src/components/pages/RegisterPage/RegisterPage.tsx
@@ -7,15 +7,15 @@ type RegisterPageProps = {
 
 const RegisterPage: React.FC<any> = () => {
   const navigate = useNavigate();
+  const [account, setAccount] = React.useState({ username: "", password: "" });
   return (
     <>
       <h1>RegisterPage</h1>
-      <button onClick={() => navigate(-1)}>Back</button>
 
       <form
         onSubmit={(e: React.FormEvent<Element>) => {
           e.preventDefault();
-          alert("Register");
+          alert(JSON.stringify(account));
         }}
       >
         <label>Username: </label>
@@ -25,8 +25,13 @@ const RegisterPage: React.FC<any> = () => {
           id="username"
           onChange={(e) => {
             console.log(e.target.value);
+            setAccount({
+              ...account,
+              username: e.target.value,
+            });
           }}
         />
+        <br />
         <label>Password: </label>
         <input
           type="text"
@@ -34,9 +39,17 @@ const RegisterPage: React.FC<any> = () => {
           id="password"
           onChange={(e) => {
             console.log(e.target.value);
+            setAccount({
+              ...account,
+              password: e.target.value,
+            });
           }}
         />
+        <br />
+        <span>Debug: {JSON.stringify(account)}</span>
+        <br />
         <button type="submit">Submit</button>
+        <button onClick={() => navigate(-1)}>Back</button>
       </form>
     </>
   );
