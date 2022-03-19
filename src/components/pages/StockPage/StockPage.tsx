e312736244b758c58a65e788fac522de966e75e5
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-03-19 14:18:13 +0700

    ep43 generate multiple element

--- src/components/pages/StockPage/StockPage.tsx
+++ src/components/pages/StockPage/StockPage.tsx
@@ -1,13 +1,19 @@
-import * as React from "react";
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
+import React from "react";
 
-export default function BasicButtons() {
+const dataArray = ["Angular", "React", "VueJS", "Flutter"];
+
+type Props = {};
+
+export default function StockPage({}: Props) {
   return (
-    <Stack spacing={2} direction="row">
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-    </Stack>
+    <>
+      <ul>
+        {dataArray.map((item, index) => (
+          <li>
+            {index + 1}. {item}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
