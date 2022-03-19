73bae6cb20d38366c9ecd03166c77e120b2594b8
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-03-19 14:35:44 +0700

    ep44 DataGrid Intro

--- src/components/pages/StockPage/StockPage.tsx
+++ src/components/pages/StockPage/StockPage.tsx
@@ -1,19 +1,39 @@
-import React from "react";
+import * as React from "react";
+import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import axios from "axios";
 
-const dataArray = ["Angular", "React", "VueJS", "Flutter"];
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 70 },
+  { field: "image", headerName: "IMAGE", width: 100 },
+  { field: "name", headerName: "NAME", width: 430 },
+  { field: "price", headerName: "PRICE", width: 130 },
+  { field: "stock", headerName: "STOCK", width: 130 },
+];
 
-type Props = {};
+// const rows = [
+//   { id: 1, name: "Snow1", price: "Jon", stock: 35 },
+//   { id: 2, name: "Snow2", price: "Jon", stock: 35 },
+//   { id: 3, name: "Snow3", price: "Jon", stock: 35 },
+// ];
 
-export default function StockPage({}: Props) {
+export default function DataTable() {
+  const [rows, setRows] = React.useState([]);
+
+  React.useEffect(() => {
+    axios.get("http://localhost:8085/api/v2/stock/product").then((result) => {
+      setRows(result.data);
+    });
+  }, []);
+
   return (
-    <>
-      <ul>
-        {dataArray.map((item, index) => (
-          <li>
-            {index + 1}. {item}
-          </li>
-        ))}
-      </ul>
-    </>
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+      />
+    </div>
   );
 }
