a73eea3f430dba8e8ab763ff94bf747dce3923e5
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-03-20 14:42:52 +0700

    ep62 Install ChartJS

--- src/components/pages/ReportPage/ReportPage.tsx
+++ src/components/pages/ReportPage/ReportPage.tsx
@@ -1,11 +1,51 @@
-import * as React from "react";
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
 
-type ReportPageProps = {
-  //
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: true,
+      text: "Chart.js Line Chart",
+    },
+  },
 };
 
-const ReportPage: React.FC<any> = () => {
-  return <div>ReportPage</div>;
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: [1000, 2000, 3000, 4000, 1000, 2000, 3000],
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+    {
+      label: "Dataset 2",
+      data: [2000, 1000, 5000, 1000, 3000, 3000, 5000],
+      borderColor: "rgb(53, 162, 235)",
+      backgroundColor: "rgba(53, 162, 235, 0.5)",
+    },
+  ],
 };
 
-export default ReportPage;
+export default function App() {
+  return <Line options={options} data={data} />;
+}
